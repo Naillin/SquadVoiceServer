4c6c6de1f3d1c7603472cd78f4de7819a16e24ef
Naillin <kamilasd@yandex.ru>
2024-10-19 18:30:59 +0500

    fix for HandleClient fun

--- SquadVoiceServer/Program.cs
+++ SquadVoiceServer/Program.cs
@@ -15,8 +15,9 @@ namespace SquadVoiceServer
 	{
 		private static TcpListener listener;
 		private static int SERVER_PORT = 5656;
-		List<Channel> channels = new List<Channel>(); // Список всех каналов
+		private static List<Channel> channels = new List<Channel>(); // Список всех каналов
 
+		Program program;
 		public static void Main(string[] args)
 		{
 			Program program = new Program();
@@ -27,8 +28,8 @@ namespace SquadVoiceServer
 			Console.WriteLine("Server started on port " + SERVER_PORT);
 
 			// Добавляем пример канала
-			program.channels.Add(new Channel { Name = "General" });
-			program.channels.Add(new Channel { Name = "Gaming" });
+			channels.Add(new Channel { Name = "General" });
+			channels.Add(new Channel { Name = "Gaming" });
 			//Channel generalChannel = program.channels.FirstOrDefault(c => c.Name == "General");
 			//if (generalChannel != null)
 			//{
@@ -52,52 +53,57 @@ namespace SquadVoiceServer
 			Program program = new Program();
 			NetworkTools networkTools = new NetworkTools();
 
-			using (NetworkStream stream = client.GetStream())
+			NetworkStream stream = client.GetStream();
+
+			string loginPass = networkTools.getData(stream, 256);
+			Console.WriteLine($"Попытка входа: {loginPass}");
+			// Простая проверка логина и пароля
+			string[] parts = loginPass.Split(':');
+			string login = parts[0];
+			string password = parts[1];
+
+			// Поиск пользователя
+			User user = jsonTools.FindUserByUsername(login, userDatabase);
+			if (user != null && user.Password == password) // Условие для правильных данных
 			{
-				string loginPass = networkTools.getData(stream, 256);
-				Console.WriteLine($"Попытка входа: {loginPass}");
-				// Простая проверка логина и пароля
-				string[] parts = loginPass.Split(':');
-				string login = parts[0];
-				string password = parts[1];
+				Console.WriteLine("Вход одобрен");
+				// Отправляем 1 (true) клиенту
+				byte[] responseData = new byte[] { (byte)1 };
+				stream.Write(responseData, 0, responseData.Length);
+			}
+			else
+			{
+				Console.WriteLine("Вход отклонен");
+				// Отправляем 0 (false) клиенту
+				byte[] responseData = new byte[] { (byte)0 };
+				stream.Write(responseData, 0, responseData.Length);
 
-				// Поиск пользователя
-				User user = jsonTools.FindUserByUsername(login, userDatabase);
-				if (user != null && user.Password == password) // Условие для правильных данных
-				{
-					Console.WriteLine("Вход одобрен");
-					// Отправляем 1 (true) клиенту
-					byte[] responseData = new byte[] { (byte)1 };
-					stream.Write(responseData, 0, responseData.Length);
-				}
-				else
-				{
-					Console.WriteLine("Вход отклонен");
-					// Отправляем 0 (false) клиенту
-					byte[] responseData = new byte[] { (byte)0 };
-					stream.Write(responseData, 0, responseData.Length);
-
-					return;
-				}
-
-				string channelName = networkTools.getData(stream, 256);
-				// Пример простого выбора канала
-				Channel selectedChannel = program.channels.FirstOrDefault(c => c.Name == channelName);
-				if (selectedChannel != null)
-				{
-					ClientHandler clientHandler = new ClientHandler(client, selectedChannel);
-					clientHandler.StartHandling();
-				}
+				return;
 			}
 
-			client.Close();
+			string channelName = networkTools.getData(stream, 256);
+			// Пример простого выбора канала
+			Channel selectedChannel = channels.FirstOrDefault(c => c.Name == channelName);
+			if (selectedChannel != null)
+			{
+				Console.WriteLine($"Клиент {user.Username} обслуживается.");
+				ClientHandler clientHandler = new ClientHandler(client, selectedChannel);
+				clientHandler.StartHandling();
+			}
 		}
 
 		string filePathConfig = "configServer.txt";
 		private void init()
 		{
-			string[] linesConfig = File.ReadAllLines(filePathConfig);
-			SERVER_PORT = Convert.ToInt32(linesConfig[0].Split('=')[1]);  // Получаем значение port
+			if (File.Exists(filePathConfig))
+			{
+				string[] linesConfig = File.ReadAllLines(filePathConfig);
+				SERVER_PORT = Convert.ToInt32(linesConfig[0].Split('=')[1]);  // Получаем значение port
+			}
+			else
+			{
+				File.WriteAllText(filePathConfig, $"port={SERVER_PORT}");
+			}
 		}
 	}
 }
